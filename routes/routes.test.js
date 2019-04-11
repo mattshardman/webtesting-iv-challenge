0eae95698214b391f625576ed0f0cac980d60d66
mattshardman <mattshardman@gmail.com>
2019-04-11 15:17:40 +0100

    add test to check env

--- routes/routes.test.js
+++ routes/routes.test.js
@@ -2,5 +2,13 @@ const supertest = require('supertest');
 const routes = require('./routes');
 
 describe('server', () => {
-    it('returns ')
+    describe('/GET endpoint', () => {
+        it('env is correct', () => {
+            expect(process.env.DB_ENV).toBe('testing');
+        });
+
+        it('returns status code 200', () => {
+            
+        });
+    });
 });
