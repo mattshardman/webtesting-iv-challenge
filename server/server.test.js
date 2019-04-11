8e3daa880e8950079272f54d24a8a2db4818a6bb
mattshardman <mattshardman@gmail.com>
2019-04-11 17:01:24 +0100

    add test to check adding multiple users

--- server/server.test.js
+++ server/server.test.js
@@ -6,6 +6,7 @@ const db = require('../data/dbConfig');
 describe('server', () => {
     afterEach(async () => {
        await db('users').truncate();
+       await db.seed.run();
     });
 
     it('env is correct', () => {
@@ -21,21 +22,21 @@ describe('server', () => {
         });
     });
 
-    // describe('/GET /api/users endpoint', () => {
-    //     it('returns correct status code and 2 users', () => {
-    //         return request(routes)
-    //             .get('/api/users')
-    //             .expect('Content-Type', /json/)
-    //             .expect(200)
-    //             .then(r => {
-    //                 expect(r.body.length).toBe(2);
-    //                 expect(r.body[0].name).toBe('Matt');
-    //             });
-    //     });
-    // });
+    describe('/GET /api/users endpoint', () => {
+        it('returns correct status code and 2 users', () => {
+            return request(routes)
+                .get('/api/users')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .then(r => {
+                    expect(r.body.length).toBe(2);
+                    expect(r.body[0].name).toBe('Matt');
+                });
+        });
+    });
 
     describe('/POST /api/users', () => {
-        it('creates a new user', () => {
+        it('creates a new user and returns correct length', () => {
             return request(routes)
                 .post('/api/users')
                 .send({ name: 'john', email: 'john@x.com' })
@@ -43,7 +44,30 @@ describe('server', () => {
                 .then(async r => {
                     const users = await Users.get();
                     const userLength = users.length;
-                    expect(userLength).toBe(1);
+                    expect(userLength).toBe(3);
+                });
+        });
+
+        it('creates 2 new users and returns correct length', () => {
+            return request(routes)
+                .post('/api/users')
+                .send({ name: 'john', email: 'john@x.com' })
+                .expect(201)
+                .then(async r => {
+                    const users = await Users.get();
+                    const userLength = users.length;
+                    expect(userLength).toBe(3);
+                })
+                .then(() => {
+                    return request(routes)
+                        .post('/api/users')
+                        .send({ name: 'bill', email: 'bill@x.com' })
+                        .expect(201);
+                })
+                .then(async r => {
+                    const users = await Users.get();
+                    const userLength = users.length;
+                    expect(userLength).toBe(4) 
                 });
         });
     });
